0de04991b0dd720ff7403b79cf592c484d618c19
M Haidar Hanif <git@mhaidarhanif.com>
2016-04-10 20:49:18 +0700

    Disable thefuck

--- setup-install.sh
+++ setup-install.sh
@@ -4,7 +4,9 @@ Setup Installation
 ==================
 
 # git
-sudo apt-get install git
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt-get install git git-lfs
+git submodule update --init --recursive --depth 1
 
 # zsh
 setopt EXTENDED_GLOB
@@ -13,11 +15,16 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
 done
 
 # node
-git clone --depth 1 https://github.com/creationix/nvm.git ~/.nvm
+# .nvm
+nvm install v4
 
 # python
+# .pyenv
+pyenv global system
 
 # ruby
+# .rbenv
+rbenv install 2.3.0
 
 # go
 
@@ -28,6 +35,9 @@ sudo apt-get install nginx
 
 # tmux
 sudo apt-get install tmux
+
+# thefuck
+# sudo pip install thefuck
 
 # sdkman
 export SDKMAN_DIR="$dotfiles/.sdkman" && curl -s get.sdkman.io | bash
